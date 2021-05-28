fe3677e3f58228864e69c93c0431b6ae8b49d64b
hibiki-koshikawa <hibiki overtheriver@gmail.com>
2021-05-28 08:37:32 +0000

    change password

--- app/model/user.rb
+++ app/model/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true, length:{ maximum: 15 }
-  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[\w\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, format: { with: /\A[\w]+@[\w]+\.[\w]+\z/i }
   
   has_secure_password
-  validates :password, length:{ in: 8..32 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
-  validates :password_confirmation, length:{ in: 8..32 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password, format: { with:  /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}\z/i }
 end
