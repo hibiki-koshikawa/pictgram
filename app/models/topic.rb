b8445e932181030fa490c66b48d181e0d0d5a240
hibiki-koshikawa <hibiki overtheriver@gmail.com>
2021-06-10 10:03:20 +0000

    favorite

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,10 @@ class Topic < ApplicationRecord
   validates :image, presence: true
   
   belongs_to :user
+  # belongs_to :topic<miss
   
   mount_uploader :image, ImageUploader
+  
+  has_many :favorites
+  has_many :favorite_users, through: :favorites, source: 'user'
 end
